206dc309206bc055954fd152b06813afd0cde624
nik.govorunencko <nik.govorunencko@yandex.ru>
2023-11-27 22:54:28 -0500

    homework on 22.11.2023

--- diving_in/homework/on_22.11.2023/Homework/Homework/Program.cs
+++ diving_in/homework/on_22.11.2023/Homework/Homework/Program.cs
@@ -4,7 +4,43 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Введите три параметра. Первый - число, второй - число, третий - тип вычесления (+; -; /; *) ");
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            string typeOfSolv = Console.ReadLine();
+            int result = 0;
+            if (typeOfSolv == "+")
+            {
+                result = num1 + num2;
+                Console.WriteLine($"{num1} + {num2} = {result}");
+            }
+            else if (typeOfSolv == "-")
+            {
+                result = num1 - num2;
+                Console.WriteLine($"{num1} - {num2} = {result}");
+            }
+            else if (typeOfSolv == "*")
+            {
+                result = num1 * num2;
+                Console.WriteLine($"{num1} * {num2} = {result}");
+            }
+            else if (typeOfSolv == "/")
+            {
+                if (num2 == 0) 
+                { 
+                    Console.WriteLine("Вы пытаетесь делить на ноль!"); 
+                    Environment.Exit(0); 
+                }
+                else
+                {
+                    result = num1 / num2;
+                    Console.WriteLine($"{num1} / {num2} = {result}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Похоже вы ввели что-то неправильно, попробуйте снова!");
+            }
         }
     }
 }
